7be7377801a85494ac5c068156d8ef3a140b0d7f
WaydinJochem <jochem7405@gmail.com>
2023-08-18 15:08:46 +0200

    Controller

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,20 +1,18 @@
-const User = require('../models/user'); 
+const Users = require('../models/user'); 
 
-exports.registerUser = async (req, res) => {
-  try {
-    const newUser = await User.register(req.body);
-    res.json(newUser);
-  } catch (err) {
-    res.status(500).json({ error: "Registration failed" });
-  }
+exports.registerUser = (req,res)=>{
+    Users.register(req,res)
 };
 
-exports.loginUser = async (req, res) => {
-  try {
-    res.json({ token: "Authentication_Token" });
-  } catch (err) {
-    res.status(401).json({ error: "Login failed" });
-  }
+exports.loginUser = (req,res)=>{
+    Users.login(req, res)
+;}
+exports.singleUser =(req, res) =>{
+    Users.fetchUser(req, res)
 };
-
-// exports.
+exports.allUsers = (req, res) =>{
+    Users.fetchUsers(req, res)
+};
+exports.deleteUsers = (req, res)=>{
+    Users.deleteUsers(req,res)
+};
